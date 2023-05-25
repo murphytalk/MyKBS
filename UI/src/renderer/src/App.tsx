bb9f30b2b047b7904df4b0e2b02724ec5f5ac47f
Mu Lu <murphytalk@gmail.com>
2023-05-26 01:16:46 +0900

    app win bg img

--- UI/src/renderer/src/App.tsx
+++ UI/src/renderer/src/App.tsx
@@ -1,10 +1,21 @@
-import type { Component } from 'solid-js'
+import { type Component } from 'solid-js'
+import bgImgUrl from './assets/kbs-bg.png'
 
 const App: Component = () => {
   return (
-    <main class="bg-blue-100 h-screen w-screen flex flex-col justify-center items-center">
-      world
-    </main>
+    <div class="flex items-center justify-center h-screen bg-white">
+      <div
+        class="bg-cover bg-center bg-no-repeat bg-white w-full h-full flex flex-col justify-center items-center"
+        style={{ 'background-image': `url(${bgImgUrl})` }}
+      >
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-1 rounded-lg transform transition duration-300 hover:scale-105">
+          My KBS
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-1 rounded-lg transform transition duration-300 hover:scale-105">
+          Free Chat
+        </button>
+      </div>
+    </div>
   )
 }
 
